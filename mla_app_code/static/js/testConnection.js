b1eeb5e9d6b9211202e91df2bd515bfdc1e5a5dd
Conor Murphy <conmurph@cisco.com>
2019-05-21 21:15:53 +0200

    Updated the look of stage1 using the Cisco UI kit. Also added required fields and error message upon unsuccessful login

---
+++ mla_app_code/static/js/testConnection.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+        $('#ccpForm').on('submit', function(event) {
+            var data = {"ipAddress":$('#ipAddress').val(),"username":$('#username').val(),"password":$('#password').val()};
+            
+            $.ajax({
+                url: "/testConnection",
+                type : "POST",
+                contentType: 'application/json',
+                dataType : 'json',
+                data : JSON.stringify(data),
+                success: function(response) {
+                    if (response.redirectURL !== undefined) {
+                        window.location.replace(response.redirectURL);
+                    } else {
+                        window.location.reload(true);
+                    }
+                },
+                error: function(error) {
+                    $("#alertBox").html(
+                        `
+                        <div class="alert alert--danger alert-dismissible fade in ">
+                            <div class="alert__icon icon-error-outline"></div>
+                            <strong>Login unsuccessful. Please try again. </strong>
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
+                        </div>
+                        `
+                    );
+            }
+            });
+            event.preventDefault();
+         
+         });
+         
+
+
+})
