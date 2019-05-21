00bc1e288b6066c4996daf7605b44c8340cbc970
Conor Murphy <conmurph@cisco.com>
2019-05-21 14:18:52 +0200

    updated export of kube config file

--- mla_app_code/mla_core_code.py
+++ mla_app_code/mla_core_code.py
@@ -26,69 +26,77 @@ app = Flask(__name__)
 
 @app.route("/stage1")
 def run_stage1():
-	return render_template('stage1.html')
+    return render_template('stage1.html')
 
 @app.route("/stage2", methods = ['POST', 'GET'])
 def run_stage2():
 
-		if request.method == 'POST':
-		
-			ccp = CCP("https://" + request.form['IP Address'],request.form['Username'],request.form['Password'])
-			
-			login = ccp.login()
+        if request.method == 'POST':
+        
+            ccp = CCP("https://" + request.form['IP Address'],request.form['Username'],request.form['Password'])
+            
+            login = ccp.login()
 
-			if not login:
-				print ("There was an issue with login: " + login.text)
-				return render_template('stage1.html')
-			else:
-				session['ccpURL'] = "https://" + request.form['IP Address']
-				session['ccpToken'] = login.cookies.get_dict()
+            if not login:
+                print ("There was an issue with login: " + login.text)
+                return render_template('stage1.html')
+            else:
+                session['ccpURL'] = "https://" + request.form['IP Address']
+                session['ccpToken'] = login.cookies.get_dict()
 
-		return render_template('stage2.html')
+        return render_template('stage2.html')
 
 @app.route("/stage3", methods = ['POST', 'GET'])
 def run_stage3():
 
-	if request.method == 'POST':
+    uuid = ""
 
-		ccp = CCP(session['ccpURL'],"","",session['ccpToken'])
+    if request.method == 'POST':
 
-		clusterName = request.form['Cluster Name']
+        ccp = CCP(session['ccpURL'],"","",session['ccpToken'])
 
-		try:
-			with open("ccpRequest.json") as json_data:
-				clusterData = json.load(json_data)
-				clusterData["name"] = clusterName
+        clusterName = request.form['Cluster Name']
+        
+        try:
+            with open("ccpRequest.json") as json_data:
+                clusterData = json.load(json_data)
+                clusterData["name"] = clusterName
 
-				response = ccp.deployCluster(clusterData)
+                #response = ccp.deployCluster(clusterData)
 
-				print(response.text)
+                #uuid = response.json()["uuid"]
 
-		except IOError as e:
-			return "I/O error({0}): {1}".format(e.errno, e.strerror)
+                print(uuid)
 
-		uuid = newCluster.json()["uuid"]
-		
-		getYaml = ccp.getConfig(uuid)
-		print("getYaml text  =  " + getYaml.text)
-		# create .kube directory to use kubectl
-		#saveConfig = open("kubeconfig.yaml","w+")
-		saveConfig = open("config","w+")
-		os.system('mkdir ~/.kube')
-		os.system('mv config ~/.kube')
-		saveConfig.write(getYaml.text)
+                uuid ="6de40233-738e-4e7f-bbf8-b6ca99a7d53c"
+                kubeConfig = ccp.getConfig(uuid)
 
-	return render_template('stage3.html')
+                print (kubeConfig.text)
+
+                if not os.path.exists(config.KUBE_CONFIG_DIR):
+                    try:
+                        os.makedirs(config.KUBE_CONFIG_DIR)
+                    except OSError as e:
+                        if e.errno != errno.EEXIST:
+                            raise
+                
+                with open(config.KUBE_CONFIG_DIR + "/config", "w") as f:
+                    f.write(kubeConfig.text)
+
+        except IOError as e:
+            return "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+    return render_template('stage3.html')
 
 
 @app.route("/stage4")
 def run_stage4():
-	os.system("./kfinstall.sh {} {} {} {} {}".format(config.GITHUB_TOKEN, config.PLATFORM, config.KS_VERSION, config.KF_VERSION, config.KFAPP))
-	return render_template('stage4.html')
+    os.system("./kfinstall.sh {} {} {} {} {}".format(config.GITHUB_TOKEN, config.PLATFORM, config.KS_VERSION, config.KF_VERSION, config.KFAPP))
+    return render_template('stage4.html')
 
 
 if __name__ == "__main__":
-	print (config.DEBUG_VERIFY)
-	app.secret_key = "4qDID0dZoQfZOdVh5BzG"
-	app.run(port=5000)
+    print (config.DEBUG_VERIFY)
+    app.secret_key = "4qDID0dZoQfZOdVh5BzG"
+    app.run(port=5000)
 
