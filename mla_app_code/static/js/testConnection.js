6ce3c1e0adb4f599af1a5a8298e507fb8700e130
Conor Murphy <conmurph@cisco.com>
2019-06-20 22:34:49 +0200

    Strip http/https from URL as this is already added on login. Added URL validation to CCP URL input box

--- mla_app_code/static/js/testConnection.js
+++ mla_app_code/static/js/testConnection.js
@@ -1,38 +1,64 @@
 $(document).ready(function(){
 
         $('#ccpLoginForm').on('submit', function(event) {
-
-            var data = {"ipAddress":$('#ipAddress').val(),"username":$('#username').val(),"password":$('#password').val()};
             
-            $.ajax({
-                url: "/testConnection",
-                type : "POST",
-                contentType: 'application/json',
-                dataType : 'json',
-                data : JSON.stringify(data),
-                success: function(response) {
+            ipAddress = $('#ipAddress').val()
+            ipAddress = ipAddress.replace(/^https?\:\/\//i, "");
 
-                    localStorage.setItem('consoleLog', $('#consoleLog').val());
-                    
-                    if (response.redirectURL) {
-                        window.location.replace(response.redirectURL);
-                    }  else {
-                        window.location.reload(true);
-                    }
-                },
-                error: function(error) {
-                    $("#alertBox").html(
-                        `
-                        <div class="alert alert--danger alert-dismissible fade in ">
-                            <div class="alert__icon icon-error-outline"></div>
-                            <strong>Login unsuccessful. Please try again. </strong>
-                            <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
-                        </div>
-                        `
-                    );
+            if (!validURL(ipAddress))
+            {
+                event.preventDefault();
+
+                $("#alertBox").html(
+                    `
+                    <div class="alert alert--danger alert-dismissible fade in ">
+                        <div class="alert__icon icon-error-outline"></div>
+                        <strong>Invalid URL. Please try again</strong>
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
+                    </div>
+                    `
+                );
+
+                return 
             }
-            });
-            event.preventDefault();
+            else{
+                var data = {"ipAddress":ipAddress,"username":$('#username').val(),"password":$('#password').val()};
+
+                $.ajax({
+                    url: "/testConnection",
+                    type : "POST",
+                    contentType: 'application/json',
+                    dataType : 'json',
+                    data : JSON.stringify(data),
+                    success: function(response) {
+                        console.log("test")
+                        localStorage.setItem('consoleLog', $('#consoleLog').val());
+                        
+                        if (response.redirectURL) {
+                            window.location.replace(response.redirectURL);
+                        }  else {
+                            window.location.reload(true);
+                        }
+                    },
+                    error: function(error) {
+                        console.log("error")
+                        $("#alertBox").html(
+                            `
+                            <div class="alert alert--danger alert-dismissible fade in ">
+                                <div class="alert__icon icon-error-outline"></div>
+                                <strong>Login unsuccessful. Please try again. </strong>
+                                <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
+                            </div>
+                            `
+                        );
+                }
+                });
+
+                event.preventDefault();
+                
+            }
+            
+            
          
          });
          
