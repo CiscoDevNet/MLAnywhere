196b8ac011a5c015f4cf1e367ce0b6327e48ba5e
Conor Murphy <conmurph@cisco.com>
2019-11-19 21:07:20 +0100

    Fixed error that wasn't updating the available vsphere clusters when the datacenter changed

--- mla_app_code/static/js/populateClusterDropdowns.js
+++ mla_app_code/static/js/populateClusterDropdowns.js
@@ -155,10 +155,9 @@ $(document).ready(function(){
 
   $("#vsphereProviders").on("change", function(event){
 
-    if( !$('#vsphereDatacenters').has('option').length > 0 ) {
-
-      dropdownDatacenters($('#vsphereProviders').val())
-
+    
+    dropdownDatacenters($('#vsphereProviders').val())
+    
       // if there's only a single datacenter then the "on change" event won't happen so we need to trigger it manually
       // need to set a timeout for a small delay otherwise it triggers before the dropdown has had a chance to populate
       setTimeout(function(){ 
@@ -167,17 +166,14 @@ $(document).ready(function(){
         }
       }, 1000);
 
-    }
-    else{
-      dropdownDatacenters($('#vsphereProviders').val())
-    }
+  
+   
 
   });
 
   $("#vsphereDatacenters").on("change", function(event){
 
-    if( !$('#vsphereClusters').has('option').length > 0 ) {
-      dropdownClusters($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
+    dropdownClusters($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
 
       // if there's only a single cluster then the "on change" event won't happen so we need to trigger it manually
       // need to set a timeout for a small delay otherwise it triggers before the dropdown has had a chance to populate
@@ -186,39 +182,21 @@ $(document).ready(function(){
           $('#vsphereClusters').trigger('change');
         }
       }, 1000);
-
-    }
-    else {
-      dropdownClusters($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
-    }
-
-    if( !$('#vsphereNetworks').has('option').length > 0 ) {
-      dropdownNetworks($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
-    }
-
-    if( !$('#vsphereDatastores').has('option').length > 0 ) {
-      dropdownDatastores($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
-    }
-
-    if( !$('#tenantImageTemplate').has('option').length > 0 ) {
-      dropdownTemplates($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
-    }
+    
+    
 
   });
 
   $("#vsphereClusters").on("change", function(event){
 
-    if( !$('#vsphereResourcePools').has('option').length > 0 ) {
-      dropdownResourcePools($('#vsphereProviders').val(),$('#vsphereDatacenters').val(),$('#vsphereClusters').val())
-    }
+    dropdownResourcePools($('#vsphereProviders').val(),$('#vsphereDatacenters').val(),$('#vsphereClusters').val())
 
-    // if there's only a single resource pool then the "on change" event won't happen so we need to trigger it manually
-      // need to set a timeout for a small delay otherwise it triggers before the dropdown has had a chance to populate
-      setTimeout(function(){ 
-        if ($('#vsphereResourcePools').has('option').length == 1){
-          $('#vsphereResourcePools').trigger('change');
-        }
-      }, 1000);
+    dropdownNetworks($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
+
+    dropdownDatastores($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
+
+    dropdownTemplates($('#vsphereProviders').val(),$('#vsphereDatacenters').val())
+
 
   });
 
@@ -239,9 +217,8 @@ $(document).ready(function(){
   vsphereProviders dropdown should be the first to be populated so do this on document ready. 
   first checking if it has already been populated
   */
-  if( !$('#vsphereProviders').has('option').length > 0 ) {
-  
-    dropdownProviders()
+
+ dropdownProviders()
 
     // if there's only a single provider then the "on change" event won't happen so we need to trigger it manually
     // need to set a timeout for a small delay otherwise it triggers before the dropdown has had a chance to populate
@@ -252,12 +229,9 @@ $(document).ready(function(){
     }, 1000);
     
     
-  }
 
-  if( !$('#vipPools').has('option').length > 0 ) {
-    dropdownVIPPools()
-  }
 
+  dropdownVIPPools()
 
 });
 
